36512035460ff5d011c7c40e5d86b5e1254767a4
Victor Volkov <vityan2504@gmail.com>
2022-08-31 16:09:21 +0300

    присвоил значения переменным, вывел данные в консоль

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptiv;
+const title = 'Название проекта';
+const screenPrice = 2000;
+const rollback = 20;
+const fullPrice = 11300;
+const adaptive = true;
 
-console.log('лог');
-alert('модалка');
+let screens = 'Простые, Сложные, Интерактивные';
+
+
+// тип данных переменных
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+// длина строки screens
+console.log(screens.length)
+
+console.log('Стоимость верстки экранов ' + screenPrice +
+' рублей\nСтоимость разработки сайта ' + fullPrice + ' рублей');
+
+// массив из строки screens
+screens = screens.toLowerCase().split(',');
+console.log(screens)
+
+console.log('Процент отката посреднику за работу ' + fullPrice * (rollback/100));
