9810d8e27091b5af9de22a0b60ee50abdc02f604
Victor Volkov <vityan2504@gmail.com>
2022-09-01 20:12:28 +0300

    Поменял значения переменных, написал проверку на скидку

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-const title = 'Название проекта';
-const screenPrice = 2000;
-const rollback = 20;
-const fullPrice = 11300;
-const adaptive = true;
+const title = prompt('Как называется ваш проект?', 'Название');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+const screenPrice = +prompt('Сколько будет стоить данная работа?', 12000);
+const rollback = 50;
+const adaptive = confirm('Нужен ли адаптив на сайте?');
+const service1 = prompt('Какой дополнительный тип услуги нужен?', 'Анимация');
+const servicePrice1 = +prompt('Какой дополнительный тип услуги нужен?', 2000);
+const service2 = prompt('Какой дополнительный тип услуги нужен?', 'Метрика');
+const servicePrice2 = +prompt('Какой дополнительный тип услуги нужен?', 1000);
 
-let screens = 'Простые, Сложные, Интерактивные';
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+
+console.log(servicePercentPrice);
+
+// условия для скидки
+
+if (fullPrice < 0) {
+   console.log('Что то пошло не так');
+}
+
+if (fullPrice > 30000) {
+   console.log('Даем скидку в 10%');
+}
+
+if (fullPrice > 15000 && fullPrice <= 30000) {
+   console.log('Даем скидку в 5%');
+}
+
+if(fullPrice >= 0 && fullPrice <= 15000) {
+   console.log('Скидка не предусмотрена');
+}
 
 
 // тип данных переменных
